f7e8f4547b3714b7b689ae5820a6c78d9d35c85c
qurbonoff11 <jonibekqurbonov570@gmail.com>
2025-07-10 23:08:38 +0500

    upgrade

--- src/js/data.js
+++ src/js/data.js
@@ -3,38 +3,38 @@
 export const dataBase = [
   {
     img: './src/assets/01-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Card Hover Effect",
+    // description: "Card Hover Effect",
     link: "./01.Card_Hover_Effect/index.html"
   },
   {
     img: './src/assets/02-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Color Generator",
+    // description: "Project description",
     link: "./02.Color_Generator/index.html"
   },
   {
     img: './src/assets/03-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Number Spinner",
+    // description: "Project description",
     link: "./04.Number_Spinner/index.html"
   },
   {
     img: './src/assets/04-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Sticky Notes App",
+    // description: "Project description",
     link: "./05.Sticky_Notes_App/index.html"
   },
   {
     img: './src/assets/05-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Ripple Effect",
+    // description: "Project description",
     link: "./06.Ripple_Effect/index.html"
   },
   {
     img: './src/assets/06-project.png',
-    title: "Project name",
-    description: "Project description",
+    title: "Button Animation",
+    // description: "Project description",
     link: "./07.Button_Click_Animation/index.html"
   },
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -20,7 +20,7 @@ dataBase.map((item) => {
       <div class="card-content">
         <div class="text">
           <h3 class="card-title">${title}</h3>
-          <p class="card-description">${description}</p>
+          <p class="card-description">${description == undefined ? '' : description}</p>
         </div>
         <a class="card-link" target="_blank" href="${link}">
           <button id="cardBtn">See Project</button>
@@ -31,4 +31,3 @@ dataBase.map((item) => {
 
   cardList.innerHTML += card
 });
-

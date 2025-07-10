fe9cdf8817cdd0b2531aa15954b359c8819c3e9e
qurbonoff11 <jonibekqurbonov570@gmail.com>
2025-07-10 13:05:03 +0500

    unfinished project

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const dataBase = [
+  {
+    img: "../assets/01-project.png",
+    title: "Project name",
+    description: "Project description",
+    link: "./01.Card_Hover_Effect/index.html"
+  }
+]
